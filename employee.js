188b409fa8ab49e859e3e03551e423da68491350
antonianwufo <antonia.nwufo@haymarket.com>
2021-06-28 18:42:46 +0100

    Commit

---
+++ employee.js
@@ -0,0 +1,19 @@
+const employee1 = {
+    name: 'mark',
+    position: 'manager'
+}
+const employee2 = {
+    name: 'antonia',
+    position: 'tech_analyst'
+}
+const employee3 = {
+    name: 'tony',
+    position: 'support'
+}
+
+console.log(employee1.name);
+console.log(employee1.position);
+console.log(employee2.name);
+console.log(employee2.position);
+console.log(employee3.name);
+console.log(employee3.position);
