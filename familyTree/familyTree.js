0b55d3593f3dd9ff48ec523f88f94377d9d40ec2
antonianwufo <antonia.nwufo@haymarket.com>
2021-06-28 17:13:41 +0100

    1st commit

---
+++ familyTree/familyTree.js
@@ -0,0 +1,38 @@
+const elizabeth = {
+    name: 'Elizabeth',
+    parents: [],
+    childOf:function () {return this.parents.map(parent => parent.name).join(' & ') || "unknown"}
+}
+const philip = {
+        name: 'Philip',
+        parents: [],
+        childOf:function () {return this.parents.map(parent => parent.name).join(' & ') || "unknown"}
+}
+const charles = {
+    name: 'Charles',
+    parents: [elizabeth,philip],
+    childOf:function () {return this.parents.map(parent => parent.name).join(' & ') || "unknown"}
+}
+const diana = {
+    name: 'Diana',
+    parents: [],
+    childOf:function () {return this.parents.map(parent => parent.name).join(' & ') || "unknown"}
+}
+const harry = {
+    name: 'Harry',
+    parents: [charles,diana],
+    childOf:function () {return this.parents.map(parent => parent.name).join(' & ') || "unknown"}
+}
+const meghan = {
+    name: 'Meghan',
+    parents: [],
+    childOf:function () {return this.parents.map(parent => parent.name).join(' & ') || "unknown"}
+}
+const archie = {
+    name: 'Archie',
+    parents: [harry,meghan],
+    childOf:function () {return this.parents.map(parent => parent.name).join(' & ') || "unknown"}
+}
+
+
+console.log(archie.childOf())
