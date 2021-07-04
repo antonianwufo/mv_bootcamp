a3b2c361e890aea9f68a274b6e1f5a69e1a2e5a7
antonianwufo <antonia.nwufo@haymarket.com>
2021-07-04 02:11:54 +0100

    Day 5 - update 2

--- Electric_Scooter/src/Scooter.js
+++ Electric_Scooter/src/Scooter.js
@@ -1,35 +1,64 @@
 class Scooter 
 {
-    constructor(id) 
+    constructor(id, battLvl, faultCode) 
     {
         if (!id) 
         {
             throw new Error('Scooter must have an ID')
         }
         this.id = id
-        this.stationId != ''
-        this.isUnavailable = false
+        this.stationId = false
+        this.battLvl = battLvl
+        this.isFaultCode = faultCode
     }
-addStationId(stationId){
-    this.stationId.push(stationId)
-}
-
-    reportBroken() 
+// addStationId(stationId){
+//     this.stationId.push(stationId)
+// }
+    isDocked()
     {
-        this.isUnavailable = true
-    }
-    charge() {
-        if (!this.isUnavailable)
-        {
-            return true
-        } else 
+        if (this.isStationId > 0)
         {
             return false
+        } else
+        {
+            return true
+        }
+    }
+    reportBroken() 
+    {
+        if (this.isFaultCode != 0)
+        {
+            return false
+        } else
+        {
+            return true
+        }
+    }
+    charge() 
+    {
+        if (this.battLvl = 100)
+        {
+            return "Fully charged"
+        } 
+        else
+        {
+            return "Needs charging"
+        }
+    }
+    availability() 
+    {
+        if (this.battLvl !== 100 || this.faultCode !== 0) 
+        {
+            return false
+        } 
+        else
+        {
+            return true
         }
     }
 }
 
-// const Scooter = new Scooter(111)
-// console.log(Scooter.id)
+//const Scooter = new Scooter(111,8)
+//console.log(Scooter.battLvl)
 
 module.exports = Scooter
--- Electric_Scooter/tests/scooter.test.js
+++ Electric_Scooter/tests/scooter.test.js
@@ -4,7 +4,7 @@ describe
 (
     'Scooter', () => 
 {
-    test('Scooter exists', () => 
+    test('Scooter has an ID', () => 
     {
         expect(() => new Scooter()).toThrowError('Scooter must have an ID')
     })
@@ -13,34 +13,65 @@ describe
         const scooter = new Scooter(111);
         expect(scooter.id).toEqual(111);
     })
-    // test('Check scooter battery level', () => 
-    // {
-    //     const scooter = new Scooter(78);
-    //     expect(scooter.battLvl).toEqual(78);
-    // })   
-    // test('Scooter is fully charged', () => 
-    // {
-    //     const scooter = new Scooter(100);
-    //     expect(scooter.battLvl).toEqual(100);
-    // })   
+    /*how can I get this to check the actual array?*/
+    test('Check scooter battery needs charging', () => 
+    {
+        const scooter = new Scooter(111,[98]);
+        expect(scooter.battLvl).toEqual([98]);
+    })   
+    test('Scooter is fully charged', () => 
+    {
+        const scooter = new Scooter(111,[100]);
+        expect(scooter.battLvl).toEqual([100]);
+    })   
+    test('Function - Check scooter battery needs charging', () => 
+    {
+        const scooter = new Scooter(111,[98]);
+        expect(scooter.charge()).toEqual("Needs charging");
+    })   
+    test('Function - Scooter is fully charged', () => 
+    {
+        const scooter = new Scooter(111,[100]);
+        expect(scooter.charge()).toEqual("Fully charged");
+    })   
     test('Scooter is working', () => 
     {
-        const scooter = new Scooter(111);
-        expect(scooter.charge()).toBeTruthy();
+        const scooter = new Scooter(111,[100],[0]);
+        expect(scooter.reportBroken()).toBeTruthy();
     })   
-    // test('Scooter is not available for hire', () => 
-    // {
-    //     const scooter = new Scooter(111);
-    //     expect(scooter.reportBroken()).toBeTruthy();
-    // })   
-    // test('Scooter is available for hire', () => 
-    // {
-    //     const scooter = new Scooter("Available");
-    //     expect(scooter.status).toEqual("Available");
-    // }) 
+    test('Scooter needs repair', () => 
+    {
+        const scooter = new Scooter(111,[100],[999]);
+        expect(scooter.reportBroken()).toBeFalsy();
+    })   
+    test('Scooter is working', () => 
+    {
+        const scooter = new Scooter(11,[100],[0]);
+        expect(scooter.reportBroken()).toBeTruthy();
+    }) 
+    test('Scooter is unavailable for hire', () => 
+    {
+        const scooter = new Scooter(111,[98],[999]);
+        expect(scooter.availability()).toBeFalsy();
+    })   
+    test('Scooter is available for hire', () => 
+    {
+        const scooter = new Scooter(111,100,1);
+        expect(scooter.availability()).toBeTruthy();
+    }) 
     test('Scooter is docked', () => 
     {
         const scooter = new Scooter(111);
-        expect(scooter.stationId).toEqual("Unavailable");
+        expect(scooter.isDocked()).toBeTruthy();
     })   
+    test('Scooter is docked', () => 
+    {
+        const scooter = new Scooter(111);
+        expect(scooter.isDocked()).toBe(false);
+    })  
+    test('Scooter is not docked - station id', () => 
+    {
+        const scooter = new Scooter(111,100,0,0);
+        expect(scooter.stationId).toBeFalsy();
+    }) 
 })
